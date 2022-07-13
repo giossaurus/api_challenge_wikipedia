099375d8809c2a29daf432fc8405211bd2531621
Giossaurus <gihhskt@gmail.com>
2022-07-13 14:54:30 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const submitButton = document.querySelector('#submit')
+const input = document.querySelector('#input')
+const error = document.querySelector('#error')
+const results = document.querySelector('#results')
+
+const disabled = () => {
+    input.disabled = true
+    submitButton.disabled = true
+}
+
+const enabled = () => {
+    input.disabled = false
+    submitButton.disabled = false
+}
